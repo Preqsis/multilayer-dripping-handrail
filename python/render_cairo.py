437527a5ec388de68a11da0e0a47952389934d18
Jiří Květoň <preqsis@gmail.com>
2021-06-16 09:41:12 +0200

    cairo renderer

--- python/render_cairo.py
+++ python/render_cairo.py
@@ -6,7 +6,11 @@ import math
 import cairo
 import h5py
 
+import matplotlib.pyplot as plt
+
 import random
+
+
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
@@ -15,8 +19,8 @@ if __name__ == "__main__":
     p.add_argument("--clear", action="store_true", help="If set => clear plots output directory")
     p.add_argument("--idim", type=int, default=25, help="")
     p.add_argument("--jdim", type=int, default=157, help="")
-    p.add_argument("--width", type=int, default=2000, help="")
-    p.add_argument("--height", type=int, default=2000, help="")
+    p.add_argument("--width", type=int, default=1500, help="")
+    p.add_argument("--height", type=int, default=1500, help="")
     p.add_argument("--first_frame", type=int, help="First frame to plot")
     p.add_argument("--last_frame", type=int, help="Last frame to plot")
     p.add_argument("--nth_frame", type=int, default=1, help="Plot every nth frame (default=100)")
@@ -28,17 +32,24 @@ if __name__ == "__main__":
 
     # data
     f =  h5py.File(args.data, "r")
-    keys, data = list(f.keys()), {}
-    first_frame = 0 if args.first_frame is None else args.first_frame
-    last_frame = len(keys) if args.last_frame is None else args.last_frame+1
+    #keys, data = list(f.keys()), {}
+    #first_frame = 0 if args.first_frame is None else args.first_frame
+    #last_frame = len(keys) if args.last_frame is None else args.last_frame+1
+
+    first_frame = args.first_frame
+    last_frame = args.last_frame
+
+    mlimit = 16.
+    cmap = plt.cm.get_cmap("magma")
 
     for frame in range(first_frame, last_frame, args.nth_frame):
-        if "data_%d" % frame in keys:
-            data = f["data_%d" % frame][()]
-        else:
-            continue
 
         print(frame)
+
+        #if "data_%d" % frame in keys:
+        data = f["data_%d" % frame][()]
+        #else:
+        #    continue
 
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, args.width, args.height)
         c = cairo.Context(surface)
@@ -61,13 +72,13 @@ if __name__ == "__main__":
 
                 m, azm = data[k][5], data[k][9] % (2. * math.pi)
 
-                cval = (m / 20.)
+                rgba = cmap(m / mlimit)
 
                 c.arc(0.5, 0.5, r, azm, azm+dphi)
-                c.set_source_rgb(cval, cval, cval)  # Solid color
+                c.set_source_rgb(rgba[0], rgba[1], rgba[2])  # Solid color
                 c.stroke()
 
             r -= dr
 
-        surface.write_to_png("/home/preqsis/Plocha/test/frame_%06d.png" % frame)
+        surface.write_to_png("%s/frame_%06d.png" % (args.output, frame))
         surface.finish()
