1d263d20079260876a06b356eb1fab0241751406
Jiří Květoň <preqsis@gmail.com>
2022-12-02 10:24:54 +0100

    ...

--- python/plot_flickering_count.py
+++ python/plot_flickering_count.py
@@ -19,11 +19,14 @@ def plot(input: str, output: str) -> None:
     data = np.load(input) 
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches(7, 7)
+    ax.xaxis.set_tick_params(width=1, length=7, direction="in")
+    ax.yaxis.set_tick_params(width=1, length=7, direction="in", which="both")
+    #  ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     ax.set_yscale("log")
     ax.set_xlabel("layer index $i$")
     ax.set_ylabel("$N_\mathrm{f}\ [\mathrm{events} \cdot \mathrm{step}^{-1}]$")
     ax.plot(data, color="black", linewidth=1, marker="o", markersize=3)
-    fig.savefig(output, format=Path(output).suffix)
+    fig.savefig(output, format=Path(output).suffix[1:])
 
 def main() -> None:
     parser = ArgumentParser()
